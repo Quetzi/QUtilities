a1dd6c87c31031a6463de418cccecf9667329602
quetzi <q@quetzi.net>
2014-05-30 14:51:23 +0100

    Update build for jenkins

--- src/main/java/net/quetzi/qutilities/references/References.java
+++ src/main/java/net/quetzi/qutilities/references/References.java
@@ -3,6 +3,8 @@ package net.quetzi.qutilities.references;
 public class References {
     public static final String NAME = "QUtilities";
     public static final String MODID = "QUtilities";
-    public static final String VERSION = "1.1";
-    public static final String BUILD = "28";
+    public static final String MAJOR = "@MAJOR@";
+    public static final String MINOR = "@MINOR@";
+    public static final String VERSION = MINOR + "." + MAJOR;
+    public static final String BUILD = "@BUILD_NUMBER@";
 }
