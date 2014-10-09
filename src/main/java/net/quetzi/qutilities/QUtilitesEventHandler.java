2be1e446a83a0f1748e3960a2da9ff121e034218
Quetzi <q@quetzi.net>
2014-10-09 12:02:48 +0100

    Remove whitelist feature, separated out to 'Whitelister'

--- src/main/java/net/quetzi/qutilities/QUtilitesEventHandler.java
+++ src/main/java/net/quetzi/qutilities/QUtilitesEventHandler.java
@@ -1,15 +1,11 @@
 package net.quetzi.qutilities;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
-import net.quetzi.qutilities.world.MovePlayer;
-import net.quetzi.qutilities.world.ScheduledSave;
-import net.quetzi.qutilities.world.Whitelist;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.WorldTickEvent;
+import net.quetzi.qutilities.world.MovePlayer;
+import net.quetzi.qutilities.world.ScheduledSave;
 
 public class QUtilitesEventHandler {
 
@@ -31,14 +27,6 @@ public class QUtilitesEventHandler {
                 }
                 prevTime = currTime;
             }
-            if ((QUtilities.whitelistEnabled || QUtilities.secondaryWhitelistEnabled) && (event.world.getWorldTime() % (QUtilities.checkInterval * 1200)) == 0) {
-                long currWLTime = event.world.getWorldTime();
-                if (currWLTime == 0) currWLTime++;
-                if (prevWLTime != currWLTime) {
-                    new Thread(new Whitelist()).start();
-                }
-                prevWLTime = currWLTime;
-            }
         }
     }
 
@@ -46,21 +34,5 @@ public class QUtilitesEventHandler {
     public void PlayerLoggedInHandler(PlayerLoggedInEvent event) {
 
         MovePlayer.processQueue(event.player.getGameProfile().getName());
-
-        if (!QUtilities.whitelistEnabled && !QUtilities.secondaryWhitelistEnabled) { return; }
-        EntityPlayer player = event.player;
-
-        if (MinecraftServer.getServer().getConfigurationManager().func_152596_g(player.getGameProfile())) {
-            QUtilities.log.info("Allowing exempt " + player.getGameProfile().getName());
-            return;
-        }
-
-        if (!QUtilities.whitelist.contains(player.getGameProfile().getName().toLowerCase())) {
-            QUtilities.log.info(player.getGameProfile().getName() + " not on whitelist.");
-            QUtilities.log.info("Blocking " + player.getGameProfile().getName());
-            ((EntityPlayerMP) player).playerNetServerHandler.kickPlayerFromServer(QUtilities.kickMessage);
-        } else {
-            QUtilities.log.info("Allowing " + player.getGameProfile().getName());
-        }
     }
 }
--- src/main/java/net/quetzi/qutilities/QUtilities.java
+++ src/main/java/net/quetzi/qutilities/QUtilities.java
@@ -14,14 +14,9 @@ import net.quetzi.qutilities.commands.CommandFixPlayerPos;
 import net.quetzi.qutilities.commands.CommandPlayerList;
 import net.quetzi.qutilities.commands.CommandTPS;
 import net.quetzi.qutilities.commands.CommandUptime;
-import net.quetzi.qutilities.commands.CommandWhitelist;
 import net.quetzi.qutilities.references.References;
-import net.quetzi.qutilities.world.Whitelist;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Mod(modid = References.MODID, name = References.NAME, version = References.VERSION + "-" + References.BUILD, acceptableRemoteVersions = "*")
 public class QUtilities {
@@ -30,13 +25,6 @@ public class QUtilities {
     public static long          startTime;
     public static boolean       savingEnabled;
     public static int           saveInterval;
-    public static String        uniqueID;
-    public static boolean       whitelistEnabled = false;
-    public static Set<String>   whitelist        = new HashSet();
-    public static int           checkInterval;
-    public static boolean       secondaryWhitelistEnabled = false;
-    public static String        secondaryWhitelistLocation;
-    public static String        kickMessage;
     public static Configuration config;
 
     @EventHandler
@@ -57,23 +45,7 @@ public class QUtilities {
         config.load();
         savingEnabled = config.get("Settings", "EnableWorldSaving", true).getBoolean(true);
         saveInterval = config.get("Settings", "SaveInterval", 5, "In minutes").getInt();
-        whitelistEnabled = config.get("Settings", "WhitelistEnabled", false).getBoolean(false);
-        secondaryWhitelistEnabled = config.get("Settings", "SecondaryWhitelistEnabled", false).getBoolean(false);
-        secondaryWhitelistLocation = config.get("Settings", "SecondaryWhitelistLocation", "http://your.url.here/whitelist.txt").getString();
-        uniqueID = config.get("Settings", "UniqueID", "CHANGEME").getString();
-        checkInterval = config.get("Settings", "CheckInterval", 10, "In minutes").getInt();
-        kickMessage = config.get("Settings", "KickMessage", "You are not a current Twitch Subscriber or Patron, if this is wrong wait a few minutes").getString();
         config.save();
-
-        if (whitelistEnabled || secondaryWhitelistEnabled) {
-            if ((uniqueID == null) || (uniqueID.equalsIgnoreCase("CHANGEME"))) {
-                log.info("Please set your unique ID in qutilities.cfg and restart your server.");
-            } else {
-                if (Whitelist.updateWhitelist()) {
-                    log.info("Whitelist loaded");
-                }
-            }
-        }
     }
 
     @EventHandler
@@ -91,6 +63,5 @@ public class QUtilities {
         event.registerServerCommand(new CommandUptime());
         event.registerServerCommand(new CommandTPS());
         event.registerServerCommand(new CommandFixPlayerPos());
-        event.registerServerCommand(new CommandWhitelist());
     }
 }
