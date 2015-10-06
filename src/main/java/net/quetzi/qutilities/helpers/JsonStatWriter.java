fc893346f4b45e343eabd7d26599f022d4f2b8bd
Quetzi <q@quetzi.net>
2015-10-06 15:57:15 +0100

    Change file location and improve json output

--- src/main/java/net/quetzi/qutilities/helpers/JsonStatWriter.java
+++ src/main/java/net/quetzi/qutilities/helpers/JsonStatWriter.java
@@ -2,13 +2,18 @@ package net.quetzi.qutilities.helpers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import net.minecraft.client.Minecraft;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.WorldServer;
 import net.quetzi.qutilities.QUtilities;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * Created by Quetzi on 06/10/15.
@@ -17,11 +22,11 @@ public class JsonStatWriter {
 
     public static boolean write() {
 
-        Gson gb = new GsonBuilder().setPrettyPrinting().create();
+        Gson gb = new GsonBuilder().create();
         Stats params = new Stats();
         try {
             String data = gb.toJson(params);
-            File jsonPath = new File(MinecraftServer.getServer().getFolderName(), "serverstats.json");
+            File jsonPath = new File(MinecraftServer.getServer().getFolderName(), ".." + File.separator + "serverstats.json");
             if (!jsonPath.exists() || jsonPath.delete()) {
                 FileUtils.writeStringToFile(jsonPath, gb.toJson(data));
                 return true;
@@ -39,20 +44,29 @@ public class JsonStatWriter {
 
 class Stats {
 
-    long                      timestamp   = System.currentTimeMillis();
-    String                    name        = MinecraftServer.getServer().getServerHostname();
-    String                    motd        = MinecraftServer.getServer().getMotd();
-    boolean                   isPvp       = MinecraftServer.getServer().isPVPEnabled();
-    String                    gamemode    = MinecraftServer.getServer().getGameType().getName();
-    String                    uptime      = SystemInfo.getUptime();
-    int                       playerCount = MinecraftServer.getServer().getCurrentPlayerCount();
-    int                       maxPlayers  = MinecraftServer.getServer().getMaxPlayers();
-    Hashtable<String, Double> tps         = new Hashtable<String, Double>();
+    long    timestamp        = System.currentTimeMillis();
+    String  name             = MinecraftServer.getServer().getServerHostname();
+    String  motd             = MinecraftServer.getServer().getMotd();
+    boolean isPvp            = MinecraftServer.getServer().isPVPEnabled();
+    String  gamemode         = MinecraftServer.getServer().getGameType().getName();
+    String  uptime           = SystemInfo.getUptime();
+    int     playerCount      = MinecraftServer.getServer().getCurrentPlayerCount();
+    int     maxPlayers       = MinecraftServer.getServer().getMaxPlayers();
+    String  minecraftVersion = MinecraftServer.getServer().getMinecraftVersion();
+
+    Map<String, String> dimensions = new HashMap<String, String>();
 
     public Stats() {
 
+        NumberFormat format = NumberFormat.getInstance();
+        format.setMaximumFractionDigits(2);
         for (WorldServer server : MinecraftServer.getServer().worldServers) {
-            tps.put(server.getWorldInfo().getWorldName(), SystemInfo.getWorldTickTime(server));
+            dimensions.put("name", server.provider.getDimensionName());
+            dimensions.put("tps", String.valueOf(SystemInfo.getDimensionTPS(server)) + "ms");
+            dimensions.put("daytime", String.valueOf(server.isDaytime()));
+            dimensions.put("raining", String.valueOf(server.getWorldInfo().isRaining()));
+            dimensions.put("thunder", String.valueOf(server.getWorldInfo().isThundering()));
+            dimensions.put("worldtime", String.valueOf(server.getWorldInfo().getWorldTime()));
         }
     }
 }
